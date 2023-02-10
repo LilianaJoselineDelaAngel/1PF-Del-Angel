eea20b7af8111a4fe081c262db93926d2fd02e7f
Liliana Joseline Del Angel Gomez <lilayos000@gmail.com>
2023-02-09 20:27:00 -0600

    Alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NuevoComponent } from './Alumnos/nuevo/nuevo.component';
+import { ListaComponent } from './Alumnos/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
